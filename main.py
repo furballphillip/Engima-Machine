4524218ef7cb2ce9bdb29905a43b0bcfb926310d
Phillip Boehm <136853335+furballphillip@users.noreply.github.com>
2025-05-16 14:23:12 -0500

    Basic rotor and reflector logic implemented

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ Rotors:     I-II-III
 Plugboard:  A-R, G-K, O-X
 Message:    A => X
 """
+from keyboard import Keyboard
+from plugboard import Plugboard
+from rotor import Rotor
+from reflector import Reflector
+
+I = Rotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "Q")
+II = Rotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", "E")
+III = Rotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", "V")
+IV = Rotor("ESOVPZJAYQUIRHXLNFTGKDCMWB", "J")
+V = Rotor("VZBRGITYUPSDNHLXAWMJQOFECK", "Z")
+A = Reflector("EJMZALYXVBWFCRQUONTSPIKHGD")
+B = Reflector("YRUHQSLDPXNGOKMIEBFZCWVJAT")
+C = Reflector("FVPJIAOYEDRZXWGCTKUQSBNMHL")
+
+print(I.forward(0))
