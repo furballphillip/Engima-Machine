5ee7109e2f3369f2a6d85ff48db830d0ceb5d386
Phillip Boehm <136853335+furballphillip@users.noreply.github.com>
2025-05-29 13:21:20 -0500

    Initial Setup of C++ Project structure
    Created and organized C++ source and header files into src/ and include/ directories.

---
+++ C++/include/Keyboard.h
@@ -0,0 +1,16 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#include <string>
+
+class Keyboard
+{
+public:
+    // Converts a letter (A-Z) into a signal (0-25)
+    int forward(char letter);
+
+    // Converts a signal (0-25) back into a letter (A-Z)
+    char backward(int signal);
+};
+
+#endif // KEYBOARD_H
